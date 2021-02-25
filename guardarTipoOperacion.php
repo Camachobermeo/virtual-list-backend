e32c8d9c477a289f84ba1d5275860df1a52c808e
Camachobermeo <joelbermeo452@gmail.com>
2021-02-24 23:43:47 -0500

    edts

--- guardarTipoOperacion.php
+++ guardarTipoOperacion.php
@@ -15,8 +15,17 @@ try {
 
   include_once "utiles/base_de_datos.php";
 
+  if ($params->esEdicion) {
+    $sentencia = $base_de_datos->prepare("UPDATE tipo_operacion 
+                                          SET (codigo, codigo_totem, descripcion) = 
+                                          (upper('$params->codigo'), '$params->codigo_totem', upper('$params->descripcion')) WHERE codigo = '$params->codigo'");
+$resultado = $sentencia->execute();  
+}  else {
   $sentencia = $base_de_datos->prepare("INSERT INTO tipo_operacion(codigo, codigo_totem, descripcion) VALUES (?, ?, ?);");
   $resultado = $sentencia->execute([strtoupper($params->codigo), strtoupper($params->codigo_totem), strtoupper($params->descripcion)]);
+}
+
+
 
   $response = new Result();
 
