1f8b5753baeb05f3d53bdd2f25cff3ec02d8a551
Mario Mario <marioramos083@gmail.com>
2021-04-14 14:59:22 -0500

    Update ticket.php

--- ticket.php
+++ ticket.php
@@ -68,69 +68,70 @@ try {
               <img src="https://drive.google.com/file/d/1j4YJqotD7-VfAEbqXTW4t7QyF6f8v_Dx/view">
               Presentar el siguiente ticket al ingresar.';
 
-    $message = "<!DOCTYPE html
-      PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
-    <html xmlns='http://www.w3.org/1999/xhtml'>
-    <head>
-      <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-      <title>A Simple Responsive HTML Email</title>
-      <style type='text/css'>
-        body {
-          margin: 0;
-          padding: 0;
-          min-width: 100% !important;
-        }
+    // $message = "<!DOCTYPE html
+    //   PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+    // <html xmlns='http://www.w3.org/1999/xhtml'>
+    // <head>
+    //   <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+    //   <title>A Simple Responsive HTML Email</title>
+    //   <style type='text/css'>
+    //     body {
+    //       margin: 0;
+    //       padding: 0;
+    //       min-width: 100% !important;
+    //     }
     
-        .content {
-          width: 100%;
-          max-width: 600px;
-        }
-      </style>
-    </head>
+    //     .content {
+    //       width: 100%;
+    //       max-width: 600px;
+    //     }
+    //   </style>
+    // </head>
     
-    <body yahoo bgcolor='#f6f8f1'>
-    <table border='1' cellpadding='0' cellspacing='0' width='100%'>
-    <tr>
-      <td width='260' valign='top'>
-        <table border='1' cellpadding='0' cellspacing='0' width='100%'>
-          <tr>
-            <td>
-              <img src='images/left.gif' alt='' width='100%' height='140' style='display: block;' />
-            </td>
-          </tr>
-          <tr>
-            <td style='padding: 25px 0 0 0;'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum
-              volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at
-              erat.
-            </td>
-          </tr>
-        </table>
-      </td>
-      <td style='font-size: 0; line-height: 0;' width='20'>
-        &nbsp;
-      </td>
-      <td width='260' valign='top'>
-        <table border='1' cellpadding='0' cellspacing='0' width='100%'>
-          <tr>
-            <td>
-              <img src='images/right.gif' alt='' width='100%' height='140' style='display: block;' />
-            </td>
-          </tr>
-          <tr>
-            <td style='padding: 25px 0 0 0;'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum
-              volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at
-              erat.
-            </td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-  </table>
-    </body>
-    
-    </html>";
+    // <body yahoo bgcolor='#f6f8f1'>
+    //     <table border='1' cellpadding='0' cellspacing='0' width='100%'>
+    //       <tr>
+    //           <td width='260' valign='top'>
+    //             <table border='1' cellpadding='0' cellspacing='0' width='100%'>
+    //               <tr>
+    //                 <td>
+    //                   <img src='images/left.gif' alt='' width='100%' height='140' style='display: block;' />
+    //                 </td>
+    //               </tr>
+    //               <tr>
+    //                 <td style='padding: 25px 0 0 0;'>
+    //                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum
+    //                   volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at
+    //                   erat.
+    //                 </td>
+    //               </tr>
+    //             </table>
+    //           </td>
+              
+    //           <td style='font-size: 0; line-height: 0;' width='20'>
+    //             &nbsp;
+    //           </td>
+
+    //           <td width='260' valign='top'>
+    //             <table border='1' cellpadding='0' cellspacing='0' width='100%'>
+    //               <tr>
+    //                 <td>
+    //                   <img src='images/right.gif' alt='' width='100%' height='140' style='display: block;' />
+    //                 </td>
+    //               </tr>
+    //               <tr>
+    //                 <td style='padding: 25px 0 0 0;'>
+    //                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum
+    //                   volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at
+    //                   erat.
+    //                 </td>
+    //               </tr>
+    //             </table>
+    //           </td>
+    //       </tr>
+    //   </table>
+    // </body>
+    // </html>";
 
     $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
     $cabeceras .= 'Content-type:  text/html; charset=iso-8859-1' . "\r\n";
