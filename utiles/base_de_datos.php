ec7ae5fdad0e9f6dcfcf9c16c7d761aa1924fe2f
MRamosObinte <39283568+MRamosObinte@users.noreply.github.com>
2022-07-02 14:29:11 -0500

    make it better

--- utiles/base_de_datos.php
+++ utiles/base_de_datos.php
@@ -1,8 +1,8 @@
 <?php
-$clave = "melli123";
-$usuario = "pi";
-$nombreBaseDeDatos = "ticket";
-$rutaServidor = "localhost";
+$clave = "5d85e10117864c5c36c4ae02493f2d167dd3a1271ce130637c999119355161e6";
+$usuario = "ryyhmkbdyklkyf";
+$nombreBaseDeDatos = "d28ppmvrq8aa89";
+$rutaServidor = "ec2-44-206-89-185.compute-1.amazonaws.com";
 $puerto = "5432";
 // $clave = "88d308eb5345f7237be7c1d94d979ac5eec7bc863a9b484b47252a737e01366f";
 // $usuario = "awubqqjaicjvpm";
